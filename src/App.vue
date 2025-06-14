d97a3e02b9e5ef2fe1dfa8137a749d63daa8eb54
Serhii Hul <owna.sgul@gmail.com>
2025-06-14 19:48:46 +0300

    init app notes

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script>
+import NoteForm from './components/NoteForm.vue'
+import NotesList from './components/NotesList.vue'
+
+export default {
+  components: {
+    NoteForm,
+    NotesList,
+  },
+
+  data() {
+    return {
+      notes: [],
+    }
+  },
+
+  methods: {
+    // addNote(content) {
+    //   this.notes.push({ content })
+    // },
+
+    removeNote(index) {
+      this.notes.splice(index, 1)
+    },
+  },
+}
+</script>
+
+<template>
+  <main class="container">
+    <div class="section">
+      <h1>Приложение для заметок</h1>
+    </div>
+
+    <div class="section">
+      <NoteForm @add-note="addNote" />
+    </div>
+
+    <div class="section">
+      <NotesList :notes="notes" @remove-note="removeNote" />
+    </div>
+  </main>
+</template>
---
+++ src/components/NoteForm.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  data() {
+    return {
+      noteContent: '',
+    }
+  },
+
+  methods: {
+    addNote() {
+      if (this.noteContent) {
+        this.$emit('add-note', this.noteContent.trim())
+        this.noteContent = ''
+      }
+    },
+  },
+}
+</script>
+
+<template>
+  <div class="note-form">
+    <textarea
+      v-model.trim="noteContent"
+      rows="4"
+      placeholder="Введите вашу заметку..."
+    />
+    <button @click="addNote">Добавить заметку</button>
+  </div>
+</template>
